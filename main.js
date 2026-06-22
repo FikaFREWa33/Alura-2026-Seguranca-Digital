a378f5a87b0af092ca1b3906862609fdaa33e440
FikaFREWa33 <pedro.arruda.anjos@escola.pr.gov.br>
2026-06-22 15:00:54 +0000

    Aula 6

--- main.js
+++ main.js
@@ -3,8 +3,29 @@ let tamanhoSenha = 12;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
+
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
+let alfabeto = '';
+if (checkbox[0].checked){
+    alfabeto = alfabeto + letrasMaiusculas;
+}
+console.log(alfabeto);
+
 geraSenha();
+
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+
+for (i=0; i < checkbox.length;i++){
+    checkbox[i].onclick = geraSenha;
+}
+
+console.log(checkbox[0].checked);
+
 campoSenha.value = letrasMaiusculas;
 
 numeroSenha.textContent = tamanhoSenha;
@@ -33,11 +54,25 @@ function aumentaTamanho(){
 }
 
 function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+    console.log(alfabeto);
     let senha = '';
     for (let i = 0; i < tamanhoSenha;i++){
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        let numeroAleatorio = Math.random()*alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas[numeroAleatorio];
+        senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
 }
