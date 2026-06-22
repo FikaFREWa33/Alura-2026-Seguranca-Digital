7b3f0d7e8fd4cf165aca2eb6d803355abb7d9c16
FikaFREWa33 <pedro.arruda.anjos@escola.pr.gov.br>
2026-06-22 14:30:13 +0000

    aula 4

---
+++ main.js
@@ -0,0 +1,27 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+numeroSenha.textContent = tamanhoSenha;
+
+console.log(botoes)
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        //tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       //tamanhoSenha = tamanhoSenha+1;
+       tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
